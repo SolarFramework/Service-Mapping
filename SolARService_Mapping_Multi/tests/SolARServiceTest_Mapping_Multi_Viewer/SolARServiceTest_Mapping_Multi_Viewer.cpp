c08f3c0b253f9ddf7aa71e2658e6df94b1670be7
Christophe Cutullic <christophe.cutullic@b-com.com>
2021-12-09 10:21:44 +0100

    Update header comments and configuration files

--- SolARService_Mapping_Multi/tests/SolARServiceTest_Mapping_Multi_Viewer/SolARServiceTest_Mapping_Multi_Viewer.cpp
+++ SolARService_Mapping_Multi/tests/SolARServiceTest_Mapping_Multi_Viewer/SolARServiceTest_Mapping_Multi_Viewer.cpp
@@ -1,6 +1,18 @@
-// Copyright (C) 2017-2019 Jonathan Müller <jonathanmueller.dev@gmail.com>
-// This file is subject to the license terms in the LICENSE file
-// found in the top-level directory of this distribution.
+/**
+ * @copyright Copyright (c) 2020 B-com http://www.b-com.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <iostream>
 
@@ -73,15 +85,19 @@ auto fnClientViewer = []() {
     if (timer_between_requests.elapsed() > DELAY_BETWEEN_REQUESTS) {
 
         timer_between_requests.restart();
-        pointClouds.clear();
-        keyframePoses.clear();
 
         LOG_INFO("Send request to Mapping service");
 
+        std::vector<SRef<CloudPoint>> newPointClouds;
+        std::vector<Transform3Df> newKeyframePoses;
+
         // Try to get point clouds and key frame poses to display
-        if (gMappingPipelineMulti->getDataForVisualization(pointClouds, keyframePoses) == FrameworkReturnCode::_SUCCESS) {
+        if (gMappingPipelineMulti->getDataForVisualization(newPointClouds, newKeyframePoses) == FrameworkReturnCode::_SUCCESS) {
 
             LOG_DEBUG("Viewer client: get point cloud and keyframe poses");
+
+            pointClouds = newPointClouds;
+            keyframePoses = newKeyframePoses;
         }
     }
 
