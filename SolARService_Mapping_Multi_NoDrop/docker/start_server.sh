d076aa741cdf58501981d0dbf86e3536d8199b8c
Christophe Cutullic <christophe.cutullic@b-com.com>
2022-03-10 16:02:46 +0100

    Add default port 80 to service URL if needed

--- SolARService_Mapping_Multi_NoDrop/docker/start_server.sh
+++ SolARService_Mapping_Multi_NoDrop/docker/start_server.sh
@@ -8,6 +8,15 @@ then
     echo "Error: You must define MAPUPDATE_SERVICE_URL env var with the MapUpdate Service URL"
     exit 1 
 else
+    ## Detect port in service URL
+    if echo "$MAPUPDATE_SERVICE_URL" | grep -q ":"
+    then
+        echo "Port is defined in MapUpdate service URL"
+    else
+        echo "No port set in MapUpdate service URL: add port 80 (default)"
+        export MAPUPDATE_SERVICE_URL="${MAPUPDATE_SERVICE_URL}:80"
+    fi
+
     echo "MAPUPDATE_SERVICE_URL defined: $MAPUPDATE_SERVICE_URL"
 fi
 
@@ -23,6 +32,15 @@ then
     echo "Error: You must define RELOCALIZATION_SERVICE_URL env var with the Relocalization Service URL"
     exit 1
 else
+    ## Detect port in service URL
+    if echo "$RELOCALIZATION_SERVICE_URL" | grep -q ":"
+    then
+        echo "Port is defined in Relocalization service URL"
+    else
+        echo "No port set in Relocalization service URL: add port 80 (default)"
+        export RELOCALIZATION_SERVICE_URL="${RELOCALIZATION_SERVICE_URL}:80"
+    fi
+
     echo "RELOCALIZATION_SERVICE_URL defined: $RELOCALIZATION_SERVICE_URL"
 fi
 
